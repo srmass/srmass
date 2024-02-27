f039c06aa3e917daeb66dbbe5c484ac10159db67
Suresh Das <suresh.das@massoftind.com>
2024-02-27 13:49:14 +0530

    nav init

---
+++ app/screens/splash/index.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function SplashScreen() {
+  return (
+    <View style={{flex: 1, backgroundColor: 'seagreen'}}>
+      <Text>SplashScreen</Text>
+    </View>
+  );
+}
