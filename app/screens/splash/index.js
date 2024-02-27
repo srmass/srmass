18ee05a3942b315fd0cb9e6ce793b6133b587612
Suresh Das <suresh.das@massoftind.com>
2024-02-27 14:07:51 +0530

    nav switch

--- app/screens/splash/index.js
+++ app/screens/splash/index.js
@@ -1,10 +1,16 @@
-import {View, Text} from 'react-native';
+import {Button, View, Text, Alert} from 'react-native';
 import React from 'react';
 
-export default function SplashScreen() {
+export default function SplashScreen({navigation}) {
   return (
     <View style={{flex: 1, backgroundColor: 'seagreen'}}>
       <Text>SplashScreen</Text>
+      <Button
+        title="Go To Signin"
+        onPress={() =>
+          navigation.navigate('SignInScreen', {name: 'SignInScreen'})
+        }
+      />
     </View>
   );
 }
